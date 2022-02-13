ee3d86c813330f7eaa3da9f57e545a577c5af31d
Md Abdullah Al Rahib <82247365+AbdullahAlRahib@users.noreply.github.com>
2022-02-13 19:01:04 +0600

    Add files via upload

---
+++ src/project/ConnectionProvider.java
@@ -0,0 +1,24 @@
+
+package project;
+import java.sql.*;
+
+/**
+ *
+ * @author User
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+       try
+       {
+         Class.forName("com.mysql.jdbc.Driver"); 
+          Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student","root","");
+          return con;
+       }
+       catch(Exception e)
+       {
+         return null;  
+       }
+    }
+    
+}
